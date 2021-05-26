ec46fd4448649a48d7dc2de60e9632006303375e
Githubd29 <60537406+Githubd29@users.noreply.github.com>
2021-05-26 22:51:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d5eff06f840b45e9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
